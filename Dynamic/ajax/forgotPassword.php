7274e2e32417031f1ab2a9b0d545836babe89960
Kateryna Podkopaieva <podkaterina@gmail.com>
2022-09-26 13:57:28 +0300

    deleted a folder that is not used

--- Dynamic/ajax/forgotPassword.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $email = $_POST['email'];
-  $subject = "Восстановление пароля";
-	$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
-	if(mail($email, $subject, $headers))
-		echo "На Ваш e-mail отправлено сообщение с интрукцией для восстановления пароля";
-	else
-		echo "Сообщение не отправлено";
-?>
--- Dynamic/blocks/rightCol.php
+++
@@ -1,8 +0,0 @@
-<div id="rightCol">
-  <div class="banner">
-    <img src="img/banner_1.jpg" alt="banner_1">
-  </div>
-  <div class="banner">
-    <img src="img/banner_2.jpg" alt="banner_2">
-  </div>
-</div>
